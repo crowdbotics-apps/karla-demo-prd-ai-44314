66205a65ebe7147290bd22e266ff49e9c830de84
Crowdbotics <team@crowdbotics.com>
2023-11-03 15:35:06 +0000

    Initial commit

---
+++ backend/karla_demo_prd_ai_44314/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for karla_demo_prd_ai_44314 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "karla_demo_prd_ai_44314.settings"
+)
+
+application = get_wsgi_application()
